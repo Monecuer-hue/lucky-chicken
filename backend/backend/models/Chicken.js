f72eb19c581b672727dbbaa096c340f0540792ff
Elshaddai <anesuelsha4@gmail.com>
2025-05-20 09:47:45 +0200

    Initial commit - Lucky Chicken website

--- backend/backend/models/Chicken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const chickenSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  color: String
-});
-
-module.exports = mongoose.model('Chicken', chickenSchema);
